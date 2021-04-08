a8f567155868a2b0f7b9b35e236f12cee93e830c
Pandraghon <pandraghon@outlook.com>
2021-04-08 23:22:19 +0200

    feat(pages): add projects page

--- routes/about.js
+++ routes/about.js
@@ -16,7 +16,7 @@ router.get('/support', function(req, res, next) {
 	res.render('about/support', { 
 		title: 'GW2Trivia', 
 		subtitle: 'Soutenir GW2Trivia', 
-		description: 'Statistiques sur Questions pour un Quaggan.',
+		description: 'Le soutiens peut être fait par différents moyens.',
 		keywords: 'questions pour un quaggan, guild wars, gw, gw2, ogden guéripierre, jeu, gw2trivia, trivia, culture, pandraghon',
 	});
 });
@@ -27,6 +27,16 @@ router.get('/legal', function(req, res, next) {
 		title: 'GW2Trivia', 
 		subtitle: 'Mentions légales', 
 		description: 'Tester vos connaissances sur l\'univers de Guild Wars en participant à Questions pour un Quaggan, un jeu présenté par Ogden Guéripierre.',
+		keywords: 'questions pour un quaggan, guild wars, gw, gw2, ogden guéripierre, jeu, gw2trivia, trivia, culture, pandraghon, mentions, legal',
+	});
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+	res.render('about/projects', { 
+		title: 'GW2Trivia', 
+		subtitle: 'Autres projets', 
+		description: 'Autres projets liés à l\'univers de Guild Wars 2.',
 		keywords: 'questions pour un quaggan, guild wars, gw, gw2, ogden guéripierre, jeu, gw2trivia, trivia, culture, pandraghon, mentions, legal',
 	});
 });
